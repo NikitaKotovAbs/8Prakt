7a2e64186ca70258c040bccd2781ac06c6ff440d
NikitaKotovAbs <112644423+NikitaKotovAbs@users.noreply.github.com>
2023-06-17 15:45:05 +0300

    Add files via upload

---
+++ WpfApp8/SerializationLibrary/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace SerializationLibrary
+{
+        public class DataSerializer
+        {
+            public void Serialize<T>(T data, string filePath)
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    serializer.Serialize(stream, data);
+                }
+            }
+
+            public T Deserialize<T>(string filePath)
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+                using (FileStream stream = new FileStream(filePath, FileMode.Open))
+                {
+                    return (T)serializer.Deserialize(stream);
+                }
+            }
+        }
+    
+}
---
+++ WpfApp8/WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SerializationLibrary;
+namespace WpfApp8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Пример использования библиотеки сериализации/десериализации
+            DataSerializer serializer = new DataSerializer();
+            string filePath = "data.xml";
+
+            // Сериализация
+            MyData data = new MyData { Name = "John Doe", Age = 30 };
+            serializer.Serialize(data, filePath);
+
+            // Десериализация
+            MyData deserializedData = serializer.Deserialize<MyData>(filePath);
+
+            MessageBox.Show($"Name: {deserializedData.Name}, Age: {deserializedData.Age}");
+        }
+    }
+    public class MyData
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
